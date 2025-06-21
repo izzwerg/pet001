e9761ec64f9d34dcfc480f55026eed907e54eaba
Oleksandr Horobiy <izzwerg.kr@gmail.com>
2025-06-21 17:47:29 +0300

    editable

--- le-olde-notebooke/src/components/Page/Content/Content.tsx
+++ le-olde-notebooke/src/components/Page/Content/Content.tsx
@@ -5,12 +5,16 @@ import type { IContentProps } from "../../../interfaces/IGeneral"
 export const Content = (props: IContentProps) => {
     return (
         <div className={css.content}>
-            <div className={clsx(
-                css.contentPage,
-                props.pref.theme === 0 && css.theme0,
-                props.pref.theme === 1 && css.theme1,
-                props.pref.theme === 2 && css.theme2
-            )}></div>
+            <div
+                className={clsx(
+                    css.contentPage,
+                    props.pref.theme === 0 && css.theme0,
+                    props.pref.theme === 1 && css.theme1,
+                    props.pref.theme === 2 && css.theme2
+                )}
+                contentEditable="true"
+            >
+            </div>
         </div >
     )
 }
